748bd17c6d4f6c8b54ee1b25bc6f9c586fa0ba35
Josh Wood <wood2jp2@gmail.com>
2018-01-15 23:55:27 -0500

    puzzle component

--- src/Components/Puzzle.js
+++ src/Components/Puzzle.js
@@ -2,23 +2,49 @@ import React, { Component } from 'react'
 
 class Puzzle extends Component {
 
+  checkState = e => {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
   constructor() {
     super()
     this.state = {
-      puzzles: ['Goldfish'],
+      puzzles: ['Goldfish', 'Hello'],
       currentPuzzle: '',
       incorrectGuesses: [],
       correctGuesses: [],
       guessesLeft: 10,
       wins: 0,
       losses: 0,
+      gameInProgress: false
     }
+  }
+
+  initiateGame = e => {
+    e.preventDefault()
+    let randomPuzzle = this.state.puzzles[Math.floor(Math.random()*this.state.puzzles.length)].split('')
+    this.setState({
+      gameInProgress: true,
+      currentPuzzle: randomPuzzle
+    })
+    console.log(this.state)
   }
 
   render() {
     return (
       <div>
         This will be your puzzle
+        { this.state.gameInProgress ? (
+          this.state.currentPuzzle.map( x => <p>_</p>)
+        ) : (
+          <button
+            name='newGameButton'
+            onClick={ e => this.initiateGame(e)}>Start a new game!</button>
+        )}
+        <button
+          name='checkState'
+          onClick={ e => this.checkState(e)}>Check State</button>
       </div>
     )
   }
